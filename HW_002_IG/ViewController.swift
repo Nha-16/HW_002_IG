651e106ad76c88754e61f3555693514ed828473a
Panha <yoeunsopanha2001@gmail.com>
2021-10-26 20:35:57 +0700

    Initial Commit

---
+++ HW_002_IG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_002_IG
+//
+//  Created by sossokleng on 26/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
